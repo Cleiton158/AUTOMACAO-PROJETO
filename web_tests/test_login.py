96173437276725e52a0e180d8e4670d768d7aeac
Cleiton Silva <josestrike2@gmail.com>
2026-05-28 14:02:32 -0300

    fix: configure selenium headless mode for github actions

--- web_tests/test_login.py
+++ web_tests/test_login.py
@@ -16,7 +16,7 @@ def test_purchase_flow():
     chrome_options = Options()
 
     # Deixe comentado para ver o navegador abrindo
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
